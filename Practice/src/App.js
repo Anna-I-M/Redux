b5d5ea66a091ef6c5ff90e42a5ac9f94f5dd0226
Anna Ilieva <ilieva.anna.a@gmail.com>
2023-06-08 21:18:52 +0200

    Exercises using Redux

---
+++ Practice/src/App.js
@@ -0,0 +1,50 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, Fragment } from "react";
+import { sendCartData } from "./store/cart-actions";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { fetchCartData } from "./store/cart-actions";
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const show = useSelector((state) => state.ui.showCart);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if(cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {show && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ Practice/src/components/Shop/ProductItem.js
@@ -0,0 +1,34 @@
+import Card from '../UI/Card';
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart';
+import classes from './ProductItem.module.css';
+
+const ProductItem = (props) => {
+  const dispatch = useDispatch();
+  const { id, title, price, description } = props;
+
+  const addItemHandler = () => {
+    dispatch(cartActions.addQuantity({
+      id,
+      title, 
+      price
+    }));
+  };
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addItemHandler}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ProductItem;
